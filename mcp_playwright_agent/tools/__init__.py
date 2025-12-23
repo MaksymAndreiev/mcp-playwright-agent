8fbd01fe4050b7ee09d1b71f076c2701d169a76a
Andreiv Maksym <maxoggyha@gmail.com>
2025-12-23 15:03:22 +0900

    add demo functions for client EDI and customer credentials to __all__ exports

--- mcp_playwright_agent/tools/__init__.py
+++ mcp_playwright_agent/tools/__init__.py
@@ -1,5 +1,5 @@
-from .edi_search import get_client_edi
-from .credentials import get_customer_credentials
+from .edi_search import get_client_edi, demo_get_client_edi
+from .credentials import get_customer_credentials, demo_get_customer_credentials
 from .date import get_current_date
 
-__all__ = ["get_client_edi", "get_customer_credentials", "get_current_date"]
+__all__ = ["get_client_edi", "get_customer_credentials", "get_current_date", "demo_get_customer_credentials", "demo_get_client_edi"]
